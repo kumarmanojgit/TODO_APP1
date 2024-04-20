32b9f300bb1649e599a5bce42e76a963aedd7da0
kumarmanojgit <krmanoj72099@gmail.com>
2024-04-20 18:26:26 +0530

    correction app file

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,7 +2,7 @@ import AppName from "./Component/AppName";
 import AddTodo from "./Component/AddTodo";
 import TodoItem1 from "./Component/TodoItem1";
 import TodoItem2 from "./Component/TodoItem2";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./App.css";
 function App() {
   return (

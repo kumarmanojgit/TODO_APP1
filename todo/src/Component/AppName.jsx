23f8844289c5dc7a0d8fe7b4d05fc130ee58bddd
kumarmanojgit <krmanoj72099@gmail.com>
2024-05-02 12:59:29 +0530

    add module css

--- todo/src/Component/AppName.jsx
+++ todo/src/Component/AppName.jsx
@@ -1,6 +1,7 @@
+import style from './AppName.module.css'
 function AppName()
 {
-  return <h1>TODO APP USING REACT</h1>
+  return <h1 className={style.todoHeading}>TODO APP USING REACT</h1>
   
 }
 export default AppName; 
--- todo/src/Component/TodoItems.jsx
+++ todo/src/Component/TodoItems.jsx
@@ -1,7 +1,8 @@
 import TodoItem from "./TodoItem";
+import style from "./TodoItems.module.css";
 function TodoItems({todoitems}){
 return<>
-  <div className="container">
+  <div className={style.itemContainer}>
   {todoitems.map(items=><TodoItem todoDate={items.duedate} todoName={items.Name}></TodoItem>)}
      </div>
 </>
